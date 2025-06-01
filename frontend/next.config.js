7213485e030812cc3779e107810b47ba3ff29e99
coderbas <your.basitbuhari@gmail.com>
2025-06-02 03:48:28 +0400

    Disable ESLint and TS checks during build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      // ✅ ignore ESLint errors during `next build`
+      // ⚠️ ignore ESLint errors during production builds
       ignoreDuringBuilds: true,
+    },
+    typescript: {
+      // ⚠️ ignore type‐checking errors during production builds
+      ignoreBuildErrors: true,
     },
   };
   
