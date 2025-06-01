028ddb4736595a70c2f5364d5790fe4a36246cf4
coderbas <your.basitbuhari@gmail.com>
2025-06-02 03:43:27 +0400

    Disable ESLint checks during build

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      // ✅ ignore ESLint errors during `next build`
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
